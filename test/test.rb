480ecde27c8122420af8427d54307a0aac19845b
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2014-12-15 21:47:22 +0000

    Añadidos más tests

--- test/test.rb
+++ test/test.rb
@@ -58,6 +58,11 @@ describe "Contenido de la web" do
     @browser = Selenium::WebDriver.for :firefox
     @web_local = 'http://localhost:9292/'
     @web = 'http://libook.herokuapp.com/' #Revisar por si no es está la dirección en Heroku
+    @r_gl = 'http://localhost:9292/sign_google/'
+    @r_gl1 = 'https://accounts.google.com/ServiceLogin?service=lso&passive=1209600&continue=https://accounts.google.com/o/oauth2/auth?scope%3Demail%2Bprofile%26response_type%3Dcode%26redirect_uri%3Dhttp://localhost:9292/auth/google_oauth2/callback%26access_type%3Doffline%26state%3D9a21786c6a12281ea4d1951858c84a8e64da84ea47d48cb6%26client_id%3D130257664242-4ubm1hg5sgpcu6bmlppon47dlmfo8vo2.apps.googleusercontent.com%26hl%3Des-ES%26from_login%3D1%26as%3D-70e344bf431d438b&ltmpl=popup&shdf=Cn4LEhF0aGlyZFBhcnR5TG9nb1VybBoADAsSFXRoaXJkUGFydHlEaXNwbGF5TmFtZRoPbGVpbmFoMTMtb2F1dGgyDAsSBmRvbWFpbhoPbGVpbmFoMTMtb2F1dGgyDAsSFXRoaXJkUGFydHlEaXNwbGF5VHlwZRoHREVGQVVMVAwSA2xzbyIUi7U7JDXhyezH2pISe2288yX5zK4oATIUiDMbwjbybQrKUJ3rBM7Uf418ULw&sarp=1&scc=1'
+    @r_tw = 'http://localhost:9292/sign_twitter/'
+    @r_fb = 'http://localhost:9292/#/'
+    @r = 'ok'
     @browser.get(@web_local) #Cambiar por @web cuando esté en Heroku
       @browser.manage.timeouts.implicit_wait = 5
   end
@@ -70,10 +75,32 @@ describe "Contenido de la web" do
     assert_equal(@web_local, @browser.current_url)
   end
 
-end
+  it "Mensaje de bienvenida" do
+    titulo = @browser.find_element(:tag_name, "h1").text
+	  assert_equal("Bienvenido a LiBook", titulo)
+  end
 
-# Rack Tests
-# describe "Contenido de la web" do
-#
-#
-# end
+  it "Invitación a usar Libook" do
+    titulo2 = @browser.find_element(:tag_name, "h2").text
+    assert_equal("Si estás cansado de no encontrar un libro en tu inmensa biblioteca, no esperes mas y únete al gestor de libros Libook.", titulo2)
+  end
+
+  it "Registro con Google" do
+    gl = @browser.find_element(:id, "google-auth").click
+    assert_equal(@r, gl)
+    #assert_equal(@r_gl1, @browser.current_url)
+  end
+
+  it "Registro con Facebook" do
+    fb = @browser.find_element(:id, "facebook-auth").click
+    assert_equal(@r, fb)
+    #assert_equal(@r_fb, @browser.current_url)
+  end
+
+  it "Registro con Twitter" do
+    tw = @browser.find_element(:id, "twitter-auth").click
+    assert_equal(@r, tw)
+    #assert_equal(@r_gl1, @browser.current_url)
+  end
+
+end
